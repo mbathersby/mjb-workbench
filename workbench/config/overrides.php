95843ae12a7598a878eab51639b91c644020f444
mbathersby <35317903+mbathersby@users.noreply.github.com>
2018-11-19 14:50:10 +1100

    Update overrides.php

--- workbench/config/overrides.php
+++ workbench/config/overrides.php
@@ -30,7 +30,7 @@ $config["oauthConfigs"]["default"] = array(
                                                 "key" => "3MVG9d8..z.hDcPLGQxR.BskW.oT3COrLww_ffIavG9oXmebZnozj.bYc_jt2u7OvqTWQ_yvV.w==",
                                                 "secret" => "3163979385997040958"
                                             ),
-                                            "ampfs--uat1.lightning.force.com" => array(
+                                            "ampfs--uat1.cs116.my.salesforce.com" => array(
                                                 "label" => "AMP-UAT1",
                                                 "key" => "3MVG9d8..z.hDcPLGQxR.BskW.oT3COrLww_ffIavG9oXmebZnozj.bYc_jt2u7OvqTWQ_yvV.w==",
                                                 "secret" => "3163979385997040958"
