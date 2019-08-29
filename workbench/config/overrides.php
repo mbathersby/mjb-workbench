93cab5610cd4fe71f588e8600b2c645affa4b8c4
mbathersby <35317903+mbathersby@users.noreply.github.com>
2019-08-29 12:57:36 +1000

    Update overrides.php

--- workbench/config/overrides.php
+++ workbench/config/overrides.php
@@ -22,7 +22,8 @@ $config["fuzzyServerUrlLookup"]["default"] = true;
 $config["oauthAppKey"]["default"] = "3MVG9d8..z.hDcPLGQxR.BskW.oT3COrLww_ffIavG9oXmebZnozj.bYc_jt2u7OvqTWQ_yvV.w==";
 $config["oauthAppSecret"]["default"] = "3163979385997040958";
 
-$config["oauthConfigs"]["default"] = array( "ampfs--augdev.cs116.my.salesforce.com" => "AMP-AUGDEV",
+$config["oauthConfigs"]["default"] = array( "ampfs--sepdev.cs151.my.salesforce.com" => "AMP-SEPDEV",
+                                            "ampfs--augdev.cs116.my.salesforce.com" => "AMP-AUGDEV",
                                             "ampfs--augqa.cs116.my.salesforce.com" => "AMP-AUGQA",
                                             "ampfs--uat1.cs116.my.salesforce.com" => "AMP-UAT1",
                                             "ampfs--datauat.cs152.my.salesforce.com" => "ADO-DATAUAT",
