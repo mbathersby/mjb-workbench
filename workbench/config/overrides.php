4922de4fe04c0ae30e0eee6e69f8fef79087a7dc
mbathersby <35317903+mbathersby@users.noreply.github.com>
2020-07-30 12:45:10 +1000

    Update overrides.php

--- workbench/config/overrides.php
+++ workbench/config/overrides.php
@@ -19,8 +19,8 @@
 
 $config["oauthRequired"]["default"] = true;
 $config["fuzzyServerUrlLookup"]["default"] = true;
-$config["oauthAppKey"]["default"] = "3MVG9d8..z.hDcPLGQxR.BskW.oT3COrLww_ffIavG9oXmebZnozj.bYc_jt2u7OvqTWQ_yvV.w==";
-$config["oauthAppSecret"]["default"] = "3163979385997040958";
+$config["oauthAppKey"]["default"] = "3MVG9n_HvETGhr3AvIZ7NpzAALFVWT5g.sGbggTW95N5E0d6WuxPYOEctzp1yoQv8a2H7LcE37aYgeMjReEQM";
+$config["oauthAppSecret"]["default"] = "B3B698ACE3976567A55DED92AA217D0623BD58868E9796BC249B0E371E8B44DB";
 
 $config["oauthConfigs"]["default"] = array( "nbn--caadev2.cs76.my.salesforce.com" => "NBN-CAADEV2",
                                             "nbn--caast2.cs76.my.salesforce.com" => "NBN-CAAST2",
