aec97a83d7a5f3053d0370c0885504a1505cc1fb
mbathersby <35317903+mbathersby@users.noreply.github.com>
2021-03-12 13:11:16 +1100

    Update login.php

--- workbench/login.php
+++ workbench/login.php
@@ -28,19 +28,19 @@ require_once "header.php";
         <input type="hidden" id="startUrl" name="startUrl" value="<?php print htmlspecialchars($c->getStartUrl(), ENT_QUOTES); ?>">
         <div id="login_type_selection" style="text-align: right;">
             
-            <?php if (!$c->isOAuthRequired()) { ?>
+            <!--?php if (!$c->isOAuthRequired()) { ?-->
                 <input type="radio" id="loginType_std" name="loginType" value="std"/>
                 <label for="loginType_std">Standard</label>
 
                 <input type="radio" id="loginType_adv" name="loginType" value="adv"/>
                 <label for="loginType_adv">Advanced</label>
             
-                <?php if ($c->isOAuthEnabled()) { ?>
+                <!--?php if ($c->isOAuthEnabled()) { ?-->
                     <input type="radio" id="loginType_oauth" name="loginType" value="oauth"/>
                     <label for="loginType_oauth">OAuth</label>
-                <?php } ?>
+                <!--?php } ?-->
             
-            <?php } ?>
+            <!--?php } ?-->
         </div>
 
         <div class="loginType_oauth">
