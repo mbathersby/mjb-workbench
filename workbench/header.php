84577ed463c7f55192697b49e54f99ee5b4ca4af
Matt Bathersby <matt.bathersby@awareservices.com>
2021-10-06 11:51:50 +1100

    no message

--- workbench/header.php
+++ workbench/header.php
@@ -175,6 +175,13 @@ if (WorkbenchConfig::get()->value("checkForLatestVersion") && extension_loaded('
                                 <div class="slds-dropdown-trigger slds-dropdown-trigger_click" id="menu">
                                     <button class="slds-button slds-button_icon slds-button_icon-border-filled" aria-haspopup="true" title="More Actions" id="menu-btn">
                                         <svg class="slds-button__icon" aria-hidden="true">
+                                            <use href="/static/assets/icons/utility-sprite/svg/symbols.svg#settings"/>
+                                        </svg>
+                                        <span class="slds-assistive-text">More Actions</span>
+                                    </button>
+
+                                    <button class="slds-button slds-button_icon slds-button_icon-border-filled" aria-haspopup="true" title="More Actions" id="menu-btn">
+                                        <svg class="slds-button__icon" aria-hidden="true">
                                             <use href="/static/assets/icons/utility-sprite/svg/symbols.svg#down"/>
                                         </svg>
                                         <span class="slds-assistive-text">More Actions</span>
@@ -184,7 +191,7 @@ if (WorkbenchConfig::get()->value("checkForLatestVersion") && extension_loaded('
                                             <?php
                                                 foreach ($GLOBALS["MENUS"] as $menu => $pages) {
 
-                                                    if($menu != "WORKBENCH"){
+                                                    if($menu != "WORKBENCH" && $menu != "Settings"){
                                                         print   "<li class=\"slds-dropdown__header slds-truncate\" title=\"$menu\" role=\"separator\">" .
                                                                     "<span>" . $menu . "</span>" .
                                                                 "</li>";   
