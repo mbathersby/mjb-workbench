c664bbb69d226aca729cae2098cccb2e08d7fc02
Matt Bathersby <matt.bathersby@awareservices.com>
2021-10-07 08:22:52 +1100

    no message

--- workbench/settings.php
+++ workbench/settings.php
@@ -196,7 +196,7 @@ foreach (WorkbenchConfig::get()->entries() as $configKey => $configValue) {
     }
 }
 
-print "<tr class='slds-p-top_medium'> <td></td> <td></td> <td></td> </tr>\n";
+print "<tr> <td class='slds-p-top_medium'>&nbsp;</td> <td></td> <td></td> </tr>\n";
 
 print $buttons;
 
