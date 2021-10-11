d146d995318091335dc7b820f7fb505f1760fe3b
Matt Bathersby <matt.bathersby@awareservices.com>
2021-10-11 15:28:36 +1100

    no message

--- workbench/static/script/tooltip.js
+++ workbench/static/script/tooltip.js
@@ -4,7 +4,21 @@ function showToolTip(content, nubPos, event){
     console.log(content);
     console.log(nubPos);
 
-    findScreenCoords(this.MouseEvent);
+    let coords = findScreenCoords(event);
+
+    const tooltip = document.createElement("div");
+    tooltip.classList.add('slds-popover slds-nubbin_' + nubPos);
+    tooltip.setAttribute('role', 'dialog');
+    tooltip.style.position = 'absolute';
+    tooltip.style.top = coords.x + 'px';
+    tooltip.style.left = coords.y + 'px';
+
+    let body = document.createElement("div");
+    body.classList.add('slds-popover__body');
+    body.innerHTML = content;
+
+    tooltip.appendChild(body);
+    document.appendChild(tooltip);
 
         /*<!-- button class="slds-button" aria-describedby="help" aria-disabled="true">Help Text</button -->
         <div class="slds-popover slds-popover_tooltip slds-nubbin_bottom-left" role="tooltip" id="help" style="position:absolute;top:-4px;left:35px">
@@ -17,6 +31,7 @@ function showToolTip(content, nubPos, event){
 function findScreenCoords(mouseEvent){
   var xpos;
   var ypos;
+
   if (mouseEvent){
     //FireFox
     xpos = mouseEvent.screenX;
@@ -29,4 +44,5 @@ function findScreenCoords(mouseEvent){
   }
   
   console.log(xpos + ':' + ypos);
+  return {'x': xpos, 'y': ypos};
 }
