3fba0e12785a80bebcc4dd2bd82188c9b75a7039
Matt Bathersby <matt.bathersby@awareservices.com>
2021-10-11 15:08:40 +1100

    no message

--- workbench/static/script/tooltip.js
+++ workbench/static/script/tooltip.js
@@ -1,13 +1,10 @@
-function init(){
-
-}
-
 function showToolTip(content, nubPos){
 
-    console.log(EventSource);
-    console.log(this);
+    console.log(this.Event);
     console.log(content);
     console.log(nubPos);
+
+    findScreenCoords(this.MouseEvent);
 
         /*<!-- button class="slds-button" aria-describedby="help" aria-disabled="true">Help Text</button -->
         <div class="slds-popover slds-popover_tooltip slds-nubbin_bottom-left" role="tooltip" id="help" style="position:absolute;top:-4px;left:35px">
@@ -17,4 +14,19 @@ function showToolTip(content, nubPos){
         </div>*/
 }
 
-init();
+function findScreenCoords(mouseEvent){
+  var xpos;
+  var ypos;
+  if (mouseEvent){
+    //FireFox
+    xpos = mouseEvent.screenX;
+    ypos = mouseEvent.screenY;
+  } 
+  else{
+    //IE
+    xpos = window.event.screenX;
+    ypos = window.event.screenY;
+  }
+  
+  console.log(xpos + ':' + ypos);
+}
